ba68fbac0b56d94d025e96e80f248803cd1600cf
Elton <68150154+aliyevelton@users.noreply.github.com>
2024-01-25 20:53:13 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+const coloredLogo = document.getElementById("logo");
+
 document.addEventListener("DOMContentLoaded", function(){
     window.addEventListener('scroll', function() {
         if (window.scrollY > 100) {
-            console.log("salam");
             document.getElementById('navbar_top').classList.add('fixed-navbar');
+            coloredLogo.src="https://www.simbrella.com/site/templates/img/logo_colored.svg";
         } else {
             document.getElementById('navbar_top').classList.remove('fixed-navbar');
+            coloredLogo.src="https://www.simbrella.com/site/templates/img/logo.svg";
         } 
     });
   }); 
